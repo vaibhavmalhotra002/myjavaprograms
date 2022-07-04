a6a0b8db8b505a54ba20346993581a6c33ee8502
Vaibhav Malhotra <76607940+vaibhavmalhotra002@users.noreply.github.com>
2022-07-04 23:31:56 +0530

    quotient

---
+++ com/Basics/QuotientRemainder.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class QuotientRemainder {
+    public static void main(String[] args) {
+        int num1= 23;
+        int num2= 24;
+
+        int quotient= num1/num2;
+        int remainder=num1%num2;
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}
