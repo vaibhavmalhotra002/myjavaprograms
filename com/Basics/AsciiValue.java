10a1de8a59b4ec3b6dfd355c262d80535af4b02b
Vaibhav Malhotra <76607940+vaibhavmalhotra002@users.noreply.github.com>
2022-07-04 23:30:05 +0530

    ascii value code added

---
+++ com/Basics/AsciiValue.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class AsciiValue {
+    public static void main(String[] args) {
+        char ch ='b';
+        int ascii= ch;
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+
+    }
+}
