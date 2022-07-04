c32eb2ca7d6ccece3b02bfbff2e5e1c78876ccc0
Vaibhav Malhotra <76607940+vaibhavmalhotra002@users.noreply.github.com>
2022-07-04 23:45:42 +0530

    power

---
+++ com/Basics/power.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class power {
+    public static void main(String[] args) {
+
+        int base = 3, exponent = 4;
+
+        long result = 1;
+
+        while (exponent != 0) {
+            result *= base;
+            --exponent;
+        }
+
+        System.out.println("Answer = " + result);
+    }
+}
